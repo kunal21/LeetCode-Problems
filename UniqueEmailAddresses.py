ceacb508cd4b7d2ca1bd181786f42b72c3f0b6c2
Kunal Mehta <kunalmehta2197@gmail.com>
2019-01-12 17:37:23 -0800

    code update

--- UniqueEmailAddresses.py
+++ UniqueEmailAddresses.py
@@ -4,10 +4,10 @@ class Solution:
         :type emails: List[str]
         :rtype: int
         """
-        l = []
+        final_answer = []
         for i in range(len(emails)):
             new_string = (emails[i][:emails[i].index("@")])
             new_string2 = emails[i][:emails[i].index('+')]
-            if (new_string2.replace('.',"")) + (emails[i][emails[i].index("@"):]) not in l:
-                l.append((new_string2.replace('.',"")) + (emails[i][emails[i].index("@"):]))
-        return (len(l))
+            if (new_string2.replace('.',"")) + (emails[i][emails[i].index("@"):]) not in final_answer:
+                final_answer.append((new_string2.replace('.',"")) + (emails[i][emails[i].index("@"):]))
+        return (len(final_answer))
